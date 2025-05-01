5fa15e298b46dd76d93176e710fc56ecc43a0c90
jalemgrapani <jalemlouisegrapani@gmail.com>
2025-05-01 11:00:42 +0800

    A04 POS

---
+++ ADET/A04/js/products.js
@@ -0,0 +1,96 @@
+var products = [
+    {
+        "category": "Donuts",
+        "contents": [
+            { "isAvailable": true, "name": "Alcapone", "price": 45, "image": "img/donuts/alcapone.png" },
+            { "isAvailable": true, "name": "Avocado", "price": 45, "image": "img/donuts/avocado.png" },
+            { "isAvailable": true, "name": "Berry Spears", "price": 45, "image": "img/donuts/berry-spears.png" },
+            { "isAvailable": true, "name": "Black Jack", "price": 45, "image": "img/donuts/black-jack.png" },
+            { "isAvailable": true, "name": "Caviar Strawberry", "price": 45, "image": "img/donuts/caviar-strawberry.png" },
+            { "isAvailable": true, "name": "Cheese Cakelicious", "price": 45, "image": "img/donuts/cheese-cakelicious.png" },
+            { "isAvailable": true, "name": "Choco Nutzy", "price": 45, "image": "img/donuts/choconutzy.png" },
+            { "isAvailable": true, "name": "Chocolate Rainbow", "price": 45, "image": "img/donuts/chocolate-rainbow.png" },
+            { "isAvailable": true, "name": "CocoLoco", "price": 45, "image": "img/donuts/coco-loco.png" },
+            { "isAvailable": true, "name": "Copa Banana", "price": 45, "image": "img/donuts/copa-banana.png" },
+            { "isAvailable": true, "name": "Don Mochino", "price": 45, "image": "img/donuts/don-mochino.png" },
+            { "isAvailable": true, "name": "Forest Glam", "price": 45, "image": "img/donuts/forest-glam.png" },
+            { "isAvailable": true, "name": "Glazzy", "price": 45, "image": "img/donuts/glazzy.png" },
+            { "isAvailable": true, "name": "Green Tease", "price": 45, "image": "img/donuts/green-tease.png" },
+            { "isAvailable": true, "name": "Heaven Berry", "price": 45, "image": "img/donuts/heaven-berry.png" },
+            { "isAvailable": true, "name": "J.Pops", "price": 45, "image": "img/donuts/jpops.png" },
+            { "isAvailable": true, "name": "Jacky Chunk", "price": 45, "image": "img/donuts/jacky-chunk.png" },
+            { "isAvailable": true, "name": "Jcoccino", "price": 45, "image": "img/donuts/jcoccino.png" },
+            { "isAvailable": true, "name": "Matcho Matcho", "price": 45, "image": "img/donuts/matcho-matcho.png" },
+            { "isAvailable": true, "name": "Meisisipi", "price": 45, "image": "img/donuts/meisissippi.png" },
+            { "isAvailable": true, "name": "Mr Green Tea", "price": 45, "image": "img/donuts/mr-green-tea.png" },
+            { "isAvailable": true, "name": "Oreology", "price": 45, "image": "img/donuts/oreology.png" },
+            { "isAvailable": true, "name": "Snow White", "price": 45, "image": "img/donuts/snow-white.png" },
+            { "isAvailable": true, "name": "Sugar Ice", "price": 45, "image": "img/donuts/sugar-ice.png" },
+            { "isAvailable": true, "name": "Tiramisu", "price": 45, "image": "img/donuts/tiramisu.png" },
+            { "isAvailable": true, "name": "White Desert", "price": 45, "image": "img/donuts/white-desert.png" },
+            { "isAvailable": true, "name": "Why Nut", "price": 45, "image": "img/donuts/why-nut.png" }
+        ]
+    },
+    {
+        "category": "J.Coffee",
+        "contents": [
+            { "isAvailable": true, "name": "Iced Jcoccino", "price": 135, "image": "img/jcoffee/ice-jcoccino.png" },
+            { "isAvailable": true, "name": "Hot Latte", "price": 130, "image": "img/jcoffee/hot-latte.png" },
+            { "isAvailable": true, "name": "Hot Americano", "price": 110, "image": "img/jcoffee/hot-americano.png" },
+            { "isAvailable": true, "name": "Hot Caramel Jcoccino", "price": 120, "image": "img/jcoffee/hot-caramel-jcoccino.png" },
+            { "isAvailable": true, "name": "Iced Latte", "price": 130, "image": "img/jcoffee/ice-latte.png" },
+            { "isAvailable": true, "name": "Iced Hazelnut Latte", "price": 140, "image": "img/jcoffee/hazelnut-latte.png" },
+            { "isAvailable": true, "name": "Hot Mocha Espresso", "price": 130, "image": "img/jcoffee/mocha-espresso-frappe.png" },
+            { "isAvailable": true, "name": "Iced Caramel Jcoccino", "price": 140, "image": "img/jcoffee/ice-caramel-jcoccino.png" },
+            { "isAvailable": true, "name": "Iced Americano", "price": 110, "image": "img/jcoffee/ice-americano.png" },
+            { "isAvailable": true, "name": "Iced Mocha Espresso Frappe", "price": 140, "image": "img/jcoffee/ice-mocha-espresso-frappe.png" },
+            { "isAvailable": true, "name": "Hot Jcoccino", "price": 130, "image": "img/jcoffee/hot-jcoccino.png" },
+            { "isAvailable": true, "name": "Hot Hazelnut Latte", "price": 135, "image": "img/jcoffee/hot-hazelnut-latte.png" }
+        ]
+    },
+    {
+        "category": "Chocolate (Beverages)",
+        "contents": [
+            { "isAvailable": true, "name": "Hot Chocolate", "price": 120, "image": "img/chocolate/hot-chocolate.png" },
+            { "isAvailable": true, "name": "Iced Chocolate", "price": 130, "image": "img/chocolate/iced-chocolate.png" }
+        ]
+    },
+    {
+        "category": "Tea",
+        "contents": [
+            { "isAvailable": true, "name": "Hot Green Tea Latte", "price": 110, "image": "img/tea/hot-green-tea-latte.png" },
+            { "isAvailable": true, "name": "Hot Tea", "price": 90, "image": "img/tea/hot-tea.png" },
+            { "isAvailable": true, "name": "Iced Green Tea Latte", "price": 115, "image": "img/tea/ice-green-tea-latte.png" },
+            { "isAvailable": true, "name": "Iced Lemon Tea", "price": 95, "image": "img/tea/iced-lemon-tea.png" },
+            { "isAvailable": true, "name": "Iced Thai Tea", "price": 105, "image": "img/tea/iced-thai-tea.png" }
+        ]
+    },
+    {
+        "category": "Frappe",
+        "contents": [
+            { "isAvailable": true, "name": "Avocado Frappe", "price": 150, "image": "img/frappe/avocado-frappe.png" },
+            { "isAvailable": true, "name": "Caramel Frappe", "price": 150, "image": "img/frappe/caramel-frappe.png" },
+            { "isAvailable": true, "name": "Choco Forest Frappe", "price": 150, "image": "img/frappe/choco-forest-frappe.png" },
+            { "isAvailable": true, "name": "Oreo Cheesecake Frappe", "price": 150, "image": "img/frappe/oreo-cheesecake-frappe.png" },
+            { "isAvailable": true, "name": "Blueberry Frappe", "price": 150, "image": "img/frappe/blueberry-frappe.png" },
+            { "isAvailable": true, "name": "Strawberry Frappe", "price": 150, "image": "img/frappe/strawberry-frappe.png" }
+        ]
+    },
+    {
+        "category": "J.Club (Donut Sandwich)",
+        "contents": [
+            { "isAvailable": true, "name": "Cheezy Rich", "price": 150, "image": "img/jclub/cheezy-rich.png" },
+            { "isAvailable": true, "name": "Katsu", "price": 140, "image": "img/jclub/katsu.png" },
+            { "isAvailable": true, "name": "Red Velvet", "price": 145, "image": "img/jclub/red-velvet.png" },
+            { "isAvailable": true, "name": "Sausage", "price": 155, "image": "img/jclub/sausage.png" }
+        ]
+    },
+    {
+        "category": "J.Cool (Frozen Yogurt)",
+        "contents": [
+            { "isAvailable": true, "name": "J.Cool Single", "price": 95, "image": "img/jcool/jcool-single.png" },
+            { "isAvailable": true, "name": "J.Cool Couple", "price": 140, "image": "img/jcool/jcool-couple.png" },
+            { "isAvailable": true, "name": "J.Cool Sharing", "price": 180, "image": "img/jcool/jcool-sharing.png" }
+        ]
+    }
+];
