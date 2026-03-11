a9fa5457ab2127447e965d9e59cca9352ddfdd9c
jalemgrapani <jalemlouisegrapani@gmail.com>
2026-03-11 15:56:42 +0800

    Minor revisions

--- ADET/A03/assets/js/about.js
+++ ADET/A03/assets/js/about.js
@@ -1,20 +1,19 @@
- var brands = [
-            { img: "alpinaaa.png", name: "Alpina" },
-            { img: "tommyy.png", name: "Tommy Hilfiger" },
-            { img: "calvinn.png", name: "Calvin Klein" },
-            { img: "coachh.png", name: "Coach" },
-            { img: "tissottt.png", name: "Tissot" }
-        ];
+var brands = [
+    { img: "alpinaaa.png", name: "Alpina" },
+    { img: "tommyy.png", name: "Tommy Hilfiger" },
+    { img: "calvinn.png", name: "Calvin Klein" },
+    { img: "coachh.png", name: "Coach" },
+    { img: "tissottt.png", name: "Tissot" }
+];
 
-        var container = document.getElementById("brandCardContainer");
+var container = document.getElementById("brandCardContainer");
 
-        for (var i = 0; i < brands.length; i++) {
-            var container = document.getElementById("brandCardContainer");
-            container.innerHTML += `
-            <div class='col-6 col-md-4 col-lg-4 mb-4'>
-                <div class='cardBrand d-flex flex-column align-items-center justify-content-center text-center p-4' style='border-radius: 10px;'>
-                <img src='../assets/img/` + brands[i].img + `' class='watchImage img-fluid' style='max-width: 150px; height: auto;'>
-                <h4 class='brand text-dark mt-3' style='font-size: 1.5rem;'>` + brands[i].name + `</h4>
-                </div>
-            </div>`;
-        }
+for (var i = 0; i < brands.length; i++) {
+    container.innerHTML += `
+    <div class='col-6 col-md-4 col-lg-2 mb-4 d-flex justify-content-center'>
+        <div class='cardBrand text-center'>
+            <img src='../assets/img/` + brands[i].img + `' class='watchImage'>
+            <h4 class='brand text-dark'>` + brands[i].name + `</h4>
+        </div>
+    </div>`;
+}
